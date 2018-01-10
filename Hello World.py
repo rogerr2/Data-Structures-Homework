4b004a65a8254af46c65cd00d91a89f2f1621558
Roger Robinson <roger2@graceland.edu>
2018-01-10 11:31:41 -0600

    add Hello World

---
+++ Hello World.py
@@ -0,0 +1,5 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> 
