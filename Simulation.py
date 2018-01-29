01acec8c435d5b1882f27320399369cc8c9a1a86
rogerr2 <35306165+rogerr2@users.noreply.github.com>
2018-01-28 22:23:25 -0600

    Add files via upload

---
+++ Simulation.py
@@ -0,0 +1,20 @@
+#Roger Robinson
+#Date: 1/28/2018
+#Simulation 1
+
+def main():
+
+    class physician():
+        def __init__(self):
+
+    class nurse():
+        def __init__(self):
+
+    class patient():
+        def __init__(self):
+
+    class examRoom():
+        def __init__(self):
+
+    class waitingRoom():
+        def __init__(self):
