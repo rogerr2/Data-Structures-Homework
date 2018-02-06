19b3d743b2827a19323190d37928bff3fcd119b6
rogerr2 <35306165+rogerr2@users.noreply.github.com>
2018-02-06 14:09:48 -0600

    Add files via upload

---
+++ Simulation 2.py
@@ -0,0 +1,61 @@
+#roger robinson
+
+import random
+import queue
+names = ["joe", "bobby", "suesan", "loretta", "grant", \
+         "jenny", "billy", "tucker", "cletus", "hunter", \
+         "gunner", "rose", "amy", "charlotte", "duke", \
+         "ricky", "bo", "duke", "jesse"]
+
+waitingRoom = []
+
+triageRoom = []
+
+
+examRoom = []
+examRoomSize = 6
+doctors = 6
+
+
+
+def callNurse():
+    """move patient from waiting room to triage room"""
+    triageRoom.append(waitingRoom.pop(0))
+    sort(triageRoom, key = patient.triageRoom)
+
+class Patient:
+    time = 0
+    def __init__(self, time):
+        self.triageNumber = random.randint(0, 100)
+        #self.name = names(random.randint(0, len(names)-1)) \
+                          #+ names(random.randint(0, len(names)))
+        self.arrivalTime = time
+        self.treatmentTime = random.randrange(15, 20)
+    def leave(self):
+        examRoom.pop(0)
+    def getExTime():
+        self.enTime = time
+        return self.enTime
+    def getEnTime():
+        self.exTime = time
+        return self.exTime
+
+def simMain():
+    pait = input("Enter number of patients: ")
+    time = 0
+    while True:
+        for i in range(pait):
+            pait -= 1
+            p = Patient()
+            waitingRoom.append(p)
+        print(waitingRoom)
+        for i in range(waitingRoom):
+            callNurse()
+        print(waitingRoom)
+        for patients in triageRoom:
+            if(len(examRoom)<6):
+                examRoom.append(patients)
+        for patients in examRoom:
+            patients.treatmentTime - 1
+            if patients.treatmentTime == 0: 
+simMain()
